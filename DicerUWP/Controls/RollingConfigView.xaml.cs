ed5ca90abf2e98af45b477bb538164001646a232
YdrMaster <ydrml@hotmail.com>
2020-08-12 19:28:27 +0800

    feat: 分离骰子配置，方便添加一键多投

---
+++ DicerUWP/Controls/RollingConfigView.xaml.cs
@@ -0,0 +1,151 @@
+﻿using DicerWinUI.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+
+namespace DicerUWP.Controls {
+    public sealed partial class RollingConfigView : UserControl {
+        public event EventHandler Changed;
+
+        private readonly RollingViewModel _viewModel = new RollingViewModel();
+
+        public RollingConfigView() {
+            InitializeComponent();
+            _viewModel.PropertyChanged += (_, e) => {
+                if (e.PropertyName == "Expression")
+                    Changed(this, null);
+            };
+        }
+
+        public IEnumerable<int> Roll() => _viewModel.Roll();
+        public string Expression => _viewModel.Expression;
+
+        private class RollingViewModel : Bindable {
+            private static readonly int[] Units = { 100, 20, 12, 10, 8, 6, 4, 3, 2, 1 };
+            private readonly int[] _values = Units.Select(x => x == 20 ? 1 : 0).ToArray();
+
+            private readonly Random _random = new Random();
+
+            private void UpdateExpression() {
+                var builder = new List<string>();
+                for (var i = 0; i < Units.Length; ++i)
+                    if (_values[i] != 0)
+                        builder.Add($"{(_values[i] > 0 ? "+" : "")}{_values[i]}{UnitToString(Units[i])}");
+                switch (builder.Count) {
+                    case 0:
+                        Expression = "0";
+                        break;
+                    case 1:
+                        builder.First().Substring(1);
+                        break;
+                    default: {
+                            var result = new StringBuilder();
+                            foreach (var item in builder)
+                                result.Append(item);
+                            Expression = result.ToString().Substring(1);
+                            break;
+                        }
+                }
+                Notify(nameof(Expression));
+            }
+
+            public string Expression { get; private set; } = "1d20";
+
+            public int D1 {
+                get => _values[9];
+                set {
+                    if (SetProperty(ref _values[9], value))
+                        UpdateExpression();
+                }
+            }
+
+            public int D2 {
+                get => _values[8];
+                set {
+                    if (SetProperty(ref _values[8], value))
+                        UpdateExpression();
+                }
+            }
+
+            public int D3 {
+                get => _values[7];
+                set {
+                    if (SetProperty(ref _values[7], value))
+                        UpdateExpression();
+                }
+            }
+
+            public int D4 {
+                get => _values[6];
+                set {
+                    if (SetProperty(ref _values[6], value))
+                        UpdateExpression();
+                }
+            }
+
+            public int D6 {
+                get => _values[5];
+                set {
+                    if (SetProperty(ref _values[5], value))
+                        UpdateExpression();
+                }
+            }
+
+            public int D8 {
+                get => _values[4];
+                set {
+                    if (SetProperty(ref _values[4], value))
+                        UpdateExpression();
+                }
+            }
+
+            public int D10 {
+                get => _values[3];
+                set {
+                    if (SetProperty(ref _values[3], value))
+                        UpdateExpression();
+                }
+            }
+
+            public int D12 {
+                get => _values[2];
+                set {
+                    if (SetProperty(ref _values[2], value))
+                        UpdateExpression();
+                }
+            }
+
+            public int D20 {
+                get => _values[1];
+                set {
+                    if (SetProperty(ref _values[1], value))
+                        UpdateExpression();
+                }
+            }
+
+            public int D100 {
+                get => _values[0];
+                set {
+                    if (SetProperty(ref _values[0], value))
+                        UpdateExpression();
+                }
+            }
+
+            public IEnumerable<int> Roll() {
+                for (var i = 0; i < Units.Length - 1; ++i) {
+                    if (_values[i] == 0) continue;
+                    var sum = 0;
+                    for (var j = 0; j < _values[i]; ++j)
+                        sum += _random.Next(1, Units[i] + 1);
+                    yield return sum;
+                }
+                if (_values.Last() != 0)
+                    yield return _values.Last();
+            }
+
+            private static string UnitToString(int value) => value == 1 ? "" : $"d{value}";
+        }
+    }
+}
--- DicerUWP/Controls/RollingView.xaml.cs
+++ DicerUWP/Controls/RollingView.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using DicerWinUI.Common;
 using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Windows.UI.Xaml;
@@ -11,18 +10,20 @@ namespace DicerUWP.Controls {
         public event EventHandler<string> Play;
         public event EventHandler Delete;
 
-        private readonly RollingViewModel _viewModel;
+        private readonly RollingViewModel _viewModel = new RollingViewModel();
+        private readonly string _title;
 
         public RollingView(string title) {
-            _viewModel = new RollingViewModel(title);
+            _title = title; 
             InitializeComponent();
+            _viewModel.Expression = RollingConfig.Expression;
         }
 
         private void Play_Click(object sender, RoutedEventArgs e) {
             var builder = new StringBuilder();
-            var list = _viewModel.Roll().ToArray();
+            var list = RollingConfig.Roll().ToArray();
             if (list.Length == 1)
-                Play(this, $"{_viewModel.Title} = {_viewModel.Expression} = {list[0]}");
+                Play(this, $"{_title} = {_viewModel.Expression} = {list[0]}");
             else {
                 foreach (var num in list.SkipLast(1))
                     builder.Append($"+{num}");
@@ -30,139 +31,22 @@ namespace DicerUWP.Controls {
                     builder.Append($"+{list.Last()}");
                 else
                     builder.Append(list.Last());
-                Play(this, $"{_viewModel.Title} = {_viewModel.Expression} = {builder.ToString().Substring(1)} = {list.Sum()}");
+                Play(this, $"{_title} = {_viewModel.Expression} = {builder.ToString().Substring(1)} = {list.Sum()}");
             }
         }
 
         private void Delete_Click(object sender, RoutedEventArgs e)
             => Delete(this, null);
 
-        private static string UnitToString(int value) => value == 1 ? "" : $"d{value}";
+        private void RollingChanged(object sender, EventArgs e)
+            => _viewModel.Expression = RollingConfig.Expression;
 
         private class RollingViewModel : Bindable {
-            private static readonly int[] Units = { 100, 20, 12, 10, 8, 6, 4, 3, 2, 1 };
-
-            private readonly Random _random = new Random();
-            private readonly int[] _values = new int[Units.Length];
-
-            public RollingViewModel(string title) {
-                Title = title;
-                _values[1] = 1;
-            }
-
-            public string Title { get; }
+            private string _expression;
 
             public string Expression {
-                get {
-                    var builder = new List<string>();
-                    for (var i = 0; i < Units.Length; ++i)
-                        if (_values[i] != 0)
-                            builder.Add($"{(_values[i] > 0 ? "+" : "")}{_values[i]}{UnitToString(Units[i])}");
-                    switch (builder.Count) {
-                        case 0:
-                            return "0";
-                        case 1:
-                            return builder.First().Substring(1);
-                        default: {
-                                var result = new StringBuilder();
-                                foreach (var item in builder)
-                                    result.Append(item);
-                                return result.ToString().Substring(1);
-                            }
-                    }
-                }
-            }
-
-            public int D1 {
-                get => _values[9];
-                set {
-                    if (SetProperty(ref _values[9], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public int D2 {
-                get => _values[8];
-                set {
-                    if (SetProperty(ref _values[8], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public int D3 {
-                get => _values[7];
-                set {
-                    if (SetProperty(ref _values[7], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public int D4 {
-                get => _values[6];
-                set {
-                    if (SetProperty(ref _values[6], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public int D6 {
-                get => _values[5];
-                set {
-                    if (SetProperty(ref _values[5], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public int D8 {
-                get => _values[4];
-                set {
-                    if (SetProperty(ref _values[4], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public int D10 {
-                get => _values[3];
-                set {
-                    if (SetProperty(ref _values[3], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public int D12 {
-                get => _values[2];
-                set {
-                    if (SetProperty(ref _values[2], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public int D20 {
-                get => _values[1];
-                set {
-                    if (SetProperty(ref _values[1], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public int D100 {
-                get => _values[0];
-                set {
-                    if (SetProperty(ref _values[0], value))
-                        Notify(nameof(Expression));
-                }
-            }
-
-            public IEnumerable<int> Roll() {
-                for (var i = 0; i < Units.Length - 1; ++i) {
-                    if (_values[i] == 0) continue;
-                    var sum = 0;
-                    for (var j = 0; j < _values[i]; ++j)
-                        sum += _random.Next(1, Units[i] + 1);
-                    yield return sum;
-                }
-                if (_values.Last() != 0)
-                    yield return _values.Last();
+                get => _expression;
+                set => SetProperty(ref _expression, value);
             }
         }
     }
