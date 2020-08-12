afdd19d69afcda59452d0f1dacea0a62951e2ae7
YdrMaster <ydrml@hotmail.com>
2020-08-12 17:41:37 +0800

    添加项目文件。

---
+++ DicerUWP/Controls/RollingBuilderView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using DicerWinUI.Common;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace DicerUWP.Controls {
+    public sealed partial class RollingBuilderView : UserControl {
+        public event EventHandler<string> NewRollingChecked;
+        private readonly RollingViewModel _viewModel = new RollingViewModel();
+
+        public RollingBuilderView() => InitializeComponent();
+
+        private void Button_Click(object sender, RoutedEventArgs _) {
+            if (string.IsNullOrWhiteSpace(_viewModel.Title)) return;
+            NewRollingChecked.Invoke(this, _viewModel.Title.Trim());
+            _viewModel.Title = "";
+        }
+
+        private class RollingViewModel : Bindable {
+            private string _title = "";
+
+            public string Title {
+                get => _title;
+                set => SetProperty(ref _title, value);
+            }
+        }
+    }
+}
---
+++ DicerUWP/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using DicerUWP.Controls;
+using DicerWinUI.Common;
+using Windows.UI.Xaml.Controls;
+
+namespace DicerUWP {
+    public sealed partial class MainPage : Page {
+        private MainPageModel _viewModel = new MainPageModel();
+
+        public MainPage() => InitializeComponent();
+
+        private void RollingBuilderView_NewRollingChecked(object sender, string e) {
+            var rollingView = new RollingView(e);
+            rollingView.Play += (object _, string title) => _viewModel.Result = title;
+            rollingView.Delete += (object rolling, System.EventArgs _) => RollingList.Items.Remove(rolling);
+            RollingList.Items.Insert(RollingList.Items.Count - 1, rollingView);
+        }
+
+        private class MainPageModel : Bindable {
+            private string _result = "0";
+
+            public string Result {
+                get => _result;
+                set => SetProperty(ref _result, value);
+            }
+        }
+    }
+}
